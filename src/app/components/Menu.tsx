35456c055442bbf26db11a558b056f33f8832eaf
Jakub Madejczyk <jbmk01@gmail.com>
2024-08-22 23:28:58 +0200

    menu changed

--- src/app/components/Menu.tsx
+++ src/app/components/Menu.tsx
@@ -1,8 +1,12 @@
 import { useFont } from "./FontLoader";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const Menu = () => {
   const font = useFont("AvgardLight");
+  const currentPath = usePathname();
+  const path = currentPath.split("/")[1];
+
   return (
     <div
       // h-screen
@@ -10,37 +14,68 @@ const Menu = () => {
     >
       <div className="border-y border-[#c0b3a3] w-full cursor-pointer">
         <Link href="/">
-          <div className="border-y border-[#c0b3a3] p-4 flex justify-center hover:text-[#ec4e39] ">
-            <h1>Main</h1>
+          <div
+            className={`border-y border-[#c0b3a3] p-4 flex justify-center
+            ${path == "" ? "text-[#ec4e39]" : ""}`}
+          >
+            <h1 className="hover:scale-105 transition duration-200 hover:text-[#ec4e39]">
+              Main
+            </h1>
           </div>
         </Link>
+
         <Link href="/about">
-          <div className="border-y border-[#c0b3a3] p-4 flex justify-center hover:text-[#ec4e39] ">
-            <h1>About</h1>
+          <div
+            className={`border-y border-[#c0b3a3] p-4 flex justify-center
+            ${path == "about" ? "text-[#ec4e39]" : ""}`}
+          >
+            <h1 className="hover:scale-105 transition duration-200 hover:text-[#ec4e39]">
+              About
+            </h1>
           </div>
         </Link>
 
         <Link href="/skills">
-          <div className="border-y border-[#c0b3a3] p-4 flex justify-center hover:text-[#ec4e39]">
-            <h1>Skills</h1>
+          <div
+            className={`border-y border-[#c0b3a3] p-4 flex justify-center
+            ${path == "skills" ? "text-[#ec4e39]" : ""}`}
+          >
+            <h1 className="hover:scale-105 transition duration-200 hover:text-[#ec4e39]">
+              Skills
+            </h1>
           </div>
         </Link>
 
         <Link href="/projects">
-          <div className="border-y border-[#c0b3a3] p-4 flex justify-center hover:text-[#ec4e39]">
-            <h1>Projects</h1>
+          <div
+            className={`border-y border-[#c0b3a3] p-4 flex justify-center
+            ${path == "projects" ? "text-[#ec4e39]" : ""}`}
+          >
+            <h1 className="hover:scale-105 transition duration-200 hover:text-[#ec4e39]">
+              Projects
+            </h1>
           </div>
         </Link>
 
         <Link href="/contact">
-          <div className="border-y border-[#c0b3a3] p-4 flex justify-center hover:text-[#ec4e39]">
-            <h1>Contact</h1>
+          <div
+            className={`border-y border-[#c0b3a3] p-4 flex justify-center
+            ${path == "contact" ? "text-[#ec4e39]" : ""}`}
+          >
+            <h1 className="hover:scale-105 transition duration-200 hover:text-[#ec4e39]">
+              Contact
+            </h1>
           </div>
         </Link>
 
         <Link href="/hireme">
-          <div className="border-y border-[#c0b3a3] p-4 flex justify-center hover:text-[#ec4e39]">
-            <h1>Hire me</h1>
+          <div
+            className={`border-y border-[#c0b3a3] p-4 flex justify-center
+            ${path == "hireme" ? "text-[#ec4e39]" : ""}`}
+          >
+            <h1 className="hover:scale-105 transition duration-200 hover:text-[#ec4e39]">
+              Hire me
+            </h1>
           </div>
         </Link>
       </div>
