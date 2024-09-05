100c5a16e0c6eef9c7146321214f90095980d124
Jakub Madejczyk <jbmk01@gmail.com>
2024-09-05 15:34:50 +0200

    fixed floating div layout

--- src/app/(pages)/skills/floating.tsx
+++ src/app/(pages)/skills/floating.tsx
@@ -11,8 +11,8 @@ const Floating = ({
   const controls = useAnimation();
   //   console.log(mousePosition.x, mousePosition.y);
   //   iconHovered ? console.log("hovered") : console.log("not hovered");
-  let small = true;
-  hoveredSkill.skillName == "" ? null : (small = false);
+  let hide = true;
+  hoveredSkill.skillName == "" ? null : (hide = false);
 
   useEffect(() => {
     const animateSequence = () => {
@@ -23,7 +23,6 @@ const Floating = ({
             ? mousePosition.x - 100
             : 0
           : 0,
-        // scale: small ? 0.3 : 1,
         transition: {
           type: "tween",
           ease: "backOut",
@@ -31,16 +30,16 @@ const Floating = ({
         },
       });
       controls.start({
-        scale: small ? 0.15 : 1,
+        scale: hide ? 0 : 1,
         transition: {
           type: "tween",
           ease: "backOut",
-          duration: 0.8,
+          duration: 0.9,
           delay: 0.05,
         },
       });
       controls.start({
-        borderRadius: small ? "5rem" : "1rem",
+        borderRadius: hide ? "5rem" : "1rem",
         transition: { ease: "easeOut", duration: 0.6 },
       });
     };
@@ -49,21 +48,23 @@ const Floating = ({
     return () => {
       controls.stop();
     };
-  }, [mousePosition, small, controls]);
+  }, [mousePosition, hide, controls]);
 
   return (
     <motion.div
       className={`pointer-events-none opacity-85 absolute w-[200px] h-[200px] bg-black  border-2
          border-textWhite text-textWhite 
-         
+         flex flex-col justify-center items-center
          `}
       animate={controls}
       //   transition={{ type: "tween", ease: "backOut", duration: 0.7 }}
+
+      initial={{ scale: 0 }}
     >
-      <p className="text-center mt-4 text-lg text-textRed font-semibold">
+      <p className="text-center  text-lg text-textRed font-semibold">
         {hoveredSkill.skillName}
       </p>
-      <p className="text-center mt-2">{hoveredSkill.skillDescription}</p>
+      <p className="text-center m-2 mt-1 ">{hoveredSkill.skillDescription}</p>
     </motion.div>
   );
 };
