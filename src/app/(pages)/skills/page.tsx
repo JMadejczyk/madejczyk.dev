68a64af54b34b62334985278a750de7acc2d821e
Jakub Madejczyk <jbmk01@gmail.com>
2024-09-05 15:34:02 +0200

    added skills descriptions

--- src/app/(pages)/skills/page.tsx
+++ src/app/(pages)/skills/page.tsx
@@ -6,26 +6,6 @@ import { useState } from "react";
 
 import { useFont } from "@/app/components/FontLoader";
 
-import typeScript from "@/../public/icons/typeScript.svg";
-import react from "@/../public/icons/react.svg";
-import javaScript from "@/../public/icons/javaScript.svg";
-import framerMotion from "@/../public/icons/framerMotion.svg";
-import nodeJs from "@/../public/icons/nodeJs.svg";
-import nextJs from "@/../public/icons/nextJs.svg";
-import html5 from "@/../public/icons/html5.svg";
-import tailwind from "@/../public/icons/tailwind.svg";
-import css from "@/../public/icons/css.svg";
-import python from "@/../public/icons/python.svg";
-import github from "@/../public/icons/github.svg";
-import expressJs from "@/../public/icons/expressJs.svg";
-import sqlite from "@/../public/icons/sqlite.svg";
-import nginx from "@/../public/icons/nginx.svg";
-import sql from "@/../public/icons/sql.svg";
-import docker from "@/../public/icons/docker.svg";
-import linux from "@/../public/icons/linux.svg";
-import git from "@/../public/icons/git.svg";
-import Floating from "./floating";
-
 const SkillsPage = () => {
   useSmoothScroll();
   const font = useFont("MontrealMedium");
@@ -112,6 +92,26 @@ const SkillsPage = () => {
 
 export default SkillsPage;
 
+import typeScript from "@/../public/icons/typeScript.svg";
+import react from "@/../public/icons/react.svg";
+import javaScript from "@/../public/icons/javaScript.svg";
+import framerMotion from "@/../public/icons/framerMotion.svg";
+import nodeJs from "@/../public/icons/nodeJs.svg";
+import nextJs from "@/../public/icons/nextJs.svg";
+import html5 from "@/../public/icons/html5.svg";
+import tailwind from "@/../public/icons/tailwind.svg";
+import css from "@/../public/icons/css.svg";
+import python from "@/../public/icons/python.svg";
+import github from "@/../public/icons/github.svg";
+import expressJs from "@/../public/icons/expressJs.svg";
+import sqlite from "@/../public/icons/sqlite.svg";
+import nginx from "@/../public/icons/nginx.svg";
+import sql from "@/../public/icons/sql.svg";
+import docker from "@/../public/icons/docker.svg";
+import linux from "@/../public/icons/linux.svg";
+import git from "@/../public/icons/git.svg";
+import Floating from "./floating";
+
 const skillImages = [
   typeScript,
   react,
@@ -137,49 +137,49 @@ const skillImages2 = [
 ];
 
 const skills = [
-  "typeScript",
-  "react",
-  "javaScript",
-  "framerMotion",
-  "nodeJs",
-  "nextJs",
-  "html5",
-  "tailwind",
-  "css",
+  "TypeScript",
+  "React",
+  "JavaScript",
+  "Framer Motion",
+  "Node.js",
+  "Next.js",
+  "HTML5",
+  "Tailwind CSS",
+  "CSS",
 ];
 
 const skillsDescriptions = [
-  "typeScript is freaking awesome",
-  "react is the best",
-  "javaScript is cool",
-  "framerMotion",
-  "nodeJs",
-  "nextJs ",
-  "html5 ",
-  "tailwind ",
-  "css deniecenjicewwniv njiverwvnjiv nijcewd",
+  "Better JavaScript with types, what more could you ask for? My projects are mostly written using it.",
+  "React is my go-to library for building user interfaces. Used it in many projects.",
+  "The language I started my journey with. I still use it in some projects. ",
+  "Great library for awesome spring physics based animations. I use it in my projects.",
+  "A runtime environment for JavaScript. Used it many times.",
+  "Great framework for React. My favourite for server-side rendering.",
+  'The "language" of the web. Used it partially in every project.',
+  "Utility-first CSS framework. I recently fall in love with it and use it daily.",
+  "Cascading Style Sheets. It doesn't need an introduction.",
 ];
 
 const skills2 = [
-  "python",
-  "github",
-  "expressJs",
-  "sqlite",
-  "nginx",
-  "sql",
-  "docker",
-  "linux",
-  "git",
+  "Python",
+  "GitHub",
+  "Express.js",
+  "SQLite",
+  "Nginx",
+  "SQL",
+  "Docker",
+  "Linux",
+  "Git",
 ];
 
 const skillsDescriptions2 = [
-  "python",
-  "github",
-  "expressJs",
-  "sqlite",
-  "nginx",
-  "sql",
-  "docker",
-  "linux",
-  "git",
+  "Great language for data science and machine learning. My favourite some time ago.",
+  "The place where my projects live. I use it daily.",
+  "Great framework for building APIs. Used it in some projects.",
+  "A simple SQL relational database engine. It allows you to store database as a file.",
+  "A web server. I use it mostly for reverse proxy.",
+  "Structured Query Language. I use it to communicate with databases.",
+  "A platform for building, shipping, and running applications in containers. Used it in some projects.",
+  "An open-source operating system. I use it on my server.",
+  "A distributed version control system. Used it in every project.",
 ];
